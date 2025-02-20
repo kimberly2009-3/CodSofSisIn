abd2f1922236d7aef9b7e533a3966007011fe7ed
kimberly2009-3 <garcia.kimberly.2024@cbtis-246.edu.mx>
2025-02-20 15:20:28 -0600

    Add files via upload

---
+++ act 6.py
@@ -0,0 +1,82 @@
+#nombre:kimberly cherlyn garcia castro
+#fecha:20/02/25
+#grupo y grado:2-J
+# nombre:tupla 
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[33]:
+
+
+tupla_1=(1,2,3,4,5)
+print(tupla_1)
+
+
+# In[31]:
+
+
+tupla_2=(26,7,8,9,10)
+print(tupla_2[2])
+
+
+# In[29]:
+
+
+tupla_1=(1,2,3,4,5)
+tupla_2=(6,7,8,9,10)
+tupla_nueva=tupla_1+tupla_2
+print(tupla_nueva)
+
+
+# In[23]:
+
+
+tulpa=('a','b','c')
+print(tulpa[0],tulpa[1],tulpa[2])
+
+
+# In[19]:
+
+
+tulpa=(1,2,3,4,5)
+print(2 in tulpa)
+
+
+# In[17]:
+
+
+tulpa=(0,1,2,3,4,5)
+print(tulpa[2:4])
+
+
+# In[13]:
+
+
+tulpa=(10,20,30,40,50)
+print(len(tupla))
+
+
+# In[7]:
+
+
+tulpa=(1,2,3,4,5)
+tupla_repetida=tupla*3
+print(tupla_repetida)
+
+
+# In[5]:
+
+
+lista=[1,2,3]
+tupla=tuple(lista)
+print(tupla)
+
+
+# In[1]:
+
+
+tupla=(5,6,7,8,15)
+print(min(tupla))
+print(max(tupla))
+
