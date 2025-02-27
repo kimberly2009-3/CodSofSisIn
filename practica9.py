b80f565e5f81b14abc2d7c3d2be3d8821b978297
kimberly2009-3 <garcia.kimberly.2024@cbtis-246.edu.mx>
2025-02-27 14:44:20 -0600

    Add files via upload

---
+++ practica9.py
@@ -0,0 +1,41 @@
+#kimberly cherlyn garcia castro
+#2-J
+#26/02/2025
+
+
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+for i in range (10):
+    print(i)
+for i in range (1,20,2):
+    print(i)
+
+lista1=[0,1,2,3,4,5,6,7,8,9]
+lista1.sort(reverse=true)
+print(lista1)
+
+
+# In[11]:
+
+
+numero=int(input("ingresa el numero que deseas multiplicar"))
+for i in range (1,11):
+      print(i,"x",numero,"=",i*numero)
+      
+
+
+# In[15]:
+
+
+numero1="1234567890"
+contador=1
+numero=input("ingresa el numero")
+numero2=list(numero)
+for numero3 in numero2:
+    contador= int (numero3)*contador
+    print("producto",contador)
+
