088ad1b94d4be3e0123744ced50ec7ce8fce182e
kimberly2009-3 <garcia.kimberly.2024@cbtis-246.edu.mx>
2025-03-06 14:52:21 -0600

    Add files via upload

---
+++ practica 5.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[17]:
+
+
+nombre = input("cual es tu nombre")
+print("hola" , nombre)
+apellido = input("cual es tu apellido")
+print("hola " , nombre, apellido)
+print("tu nombre invertido es " ,nombre[::-1])
+print("tu apellido invertido es " , apellido[::-1])
+
